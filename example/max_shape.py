fcd3825659494c7551df75cabbc2db8f00fab6d2
Dominic Jack <thedomjack@gmail.com>
2018-06-05 16:19:21 +1000

    added example

---
+++ example/max_shape.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from pascal_voc.dataset import PascalVocDataset
+# from pascal_voc.seg import get_colors, image_to_seg
+
+# colors = get_colors()
+
+with PascalVocDataset(mode='val', year=2012) as ds:
+    max_h, max_w = 0, 0
+    for key in ds:
+        example = ds[key]
+        seg = example.load_class_segmentation()
+        h, w = seg.size
+        max_h = max(h, max_h)
+        max_w = max(w, max_w)
+
+print(max_h, max_w)
