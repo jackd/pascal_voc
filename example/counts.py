a50dd62385ae69f34c1395d6d9aaef8dd8aee45c
Dominic Jack <thedomjack@gmail.com>
2018-06-09 16:14:42 +1000

    updated to include combined

--- example/counts.py
+++ example/counts.py
@@ -3,7 +3,9 @@ from pascal_voc.core import get_voc_data
 
 for key, modes in (
         ('base', ('train', 'val', 'trainval')),
-        ('augmented', ('train', 'val'))):
+        ('augmented', ('train', 'val')),
+        ('combined', ('train', 'val')),
+        ):
     for mode in modes:
         with get_voc_data(key, mode=mode) as data:
             print('%s - %s: %d' % (key, mode, len(data.get_example_ids())))
